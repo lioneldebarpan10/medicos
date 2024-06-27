530fe42fe0369fb55611932f55c002afa0e69d69
lioneldebarpan10 <161072133+lioneldebarpan10@users.noreply.github.com>
2024-06-27 21:25:55 +0530

    added Javascript file

---
+++ script.js
@@ -0,0 +1,15 @@
+/*JS START*/
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
+
+/*JS END*/
